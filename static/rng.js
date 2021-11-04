d04f60666badda3b83cd56ece35097893027ce0c
Daniel Masson <danieljmasson@gmail.com>
2021-11-04 10:18:01 +0000

    Remove sorting

--- static/rng.js
+++ static/rng.js
@@ -1,23 +1,20 @@
 function luckyDip() {
 
-    let values = new Set();
+    var values = new Set();
 
     while (values.size < 6) {
         let randomBuffer = new Uint32Array(1);
         window.crypto.getRandomValues(randomBuffer);
         let randomNumber = randomBuffer[0] / (0xFFFFFFFF);
         let min = Math.ceil(1);
-        let max = Math.floor(100);
+        let max = Math.floor(60);
         let value = Math.floor(randomNumber * (max - min + 1)) + min;
         values.add(value)
     }
 
-    let a = Array.from(values);
-    a.sort(function (a, b) {
-        return a - b;
-    });
+    values = Array.from(values)
 
     for (let i = 0; i < 6; i++) {
-        document.getElementById("no" + (i + 1)).value = a[i];
+        document.getElementById("no" + (i + 1)).value = values[i];
     }
 }
